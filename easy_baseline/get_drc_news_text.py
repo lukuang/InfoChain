6daf8c97fe3e1cbdde44b6df11ff3f509e8e5c68
Kuang Lu <lukuang1989@gmail.com>
2017-11-01 09:53:56 -0400

    convert html of DRC news to trec text format

---
+++ easy_baseline/get_drc_news_text.py
@@ -0,0 +1,29 @@
+"""
+get the text of all disaster news of the DRC crawl
+"""
+
+import os
+import json
+import sys
+import re
+import argparse
+import codecs
+sys.path.append("/infolab/node4/lukuang/code/InfoChain")
+from data.drc_data.news_data import DRCNews
+from data.drc_data.query import DRCQuery
+
+def main():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("dest_dir")
+    parser.add_argument("--day_limit","-dl",type=int,default=60)
+    args=parser.parse_args()
+
+    drc_queries =  DRCQuery()
+    for qid in drc_queries.queries:
+        print "Generating text for disaster:%s" %(drc_queries.queries[qid])
+        single_disaster_news = DRCNews(qid,args.dest_dir,args.day_limit)
+        single_disaster_news.write_in_trec_format()
+
+if __name__=="__main__":
+    main()
+
