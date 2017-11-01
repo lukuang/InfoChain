4b442b1e0a2da2b8496930c0fbc777b66b7dc917
Kuang Lu <lukuang1989@gmail.com>
2017-10-31 20:02:53 -0400

    add initial code for processing drc data

---
+++ data/drc_data/query.py
@@ -0,0 +1,19 @@
+"""
+process the DRC query data
+"""
+
+import os
+import json
+import sys
+import re
+import argparse
+import codecs
+
+def main():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("")
+    args=parser.parse_args()
+
+if __name__=="__main__":
+    main()
+
---
+++ data/drc_data/twitter_data.py
@@ -0,0 +1,19 @@
+"""
+process twitter data of crawled drc data
+"""
+
+import os
+import json
+import sys
+import re
+import argparse
+import codecs
+
+def main():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("")
+    args=parser.parse_args()
+
+if __name__=="__main__":
+    main()
+
