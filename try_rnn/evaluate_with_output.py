997d17cd8febfad67f570e1be54452961ea5195d
Kuang Lu <lukuang1989@gmail.com>
2018-02-25 18:16:50 -0500

    add option to specify a required target word and show the output word distribution of the target word

--- try_rnn/evaluate_with_output.py
+++ try_rnn/evaluate_with_output.py
@@ -16,6 +16,8 @@ from torch.autograd import Variable
 from collections import namedtuple
 import data
 
+from random import randint
+
 
 DICTIONARY = {}
 
@@ -58,6 +60,13 @@ def printnorm(self, input, output):
     print('output norm:', output.data.norm())
     # print asdf
 
+def check_tagret(targets,required_word_index,position,eval_batch_size,bptt):
+    for batch_position in range(eval_batch_size):
+        for required_word_position in range(bptt):
+            if targets[batch_position+required_word_position*eval_batch_size] == required_word_index:
+                return required_word_position,batch_position
+    return None, None
+
 def evaluate_all(data_source,model,corpus,bptt,eval_batch_size):
     # Turn on evaluation mode which disables dropout.
     model.eval()
@@ -75,7 +84,7 @@ def evaluate_all(data_source,model,corpus,bptt,eval_batch_size):
         hidden = repackage_hidden(hidden)
     return total_loss[0] / len(data_source)
 
-def evaluate_small_sample(data_source,model,corpus,bptt,eval_batch_size):
+def evaluate_small_sample(data_source,model,corpus,bptt,eval_batch_size,required_word_index, position,index):
     # Turn on evaluation mode which disables dropout.
     model.eval()
     criterion = nn.CrossEntropyLoss()
@@ -83,42 +92,63 @@ def evaluate_small_sample(data_source,model,corpus,bptt,eval_batch_size):
     ntokens = len(corpus.dictionary)
     idx2word = corpus.dictionary.idx2word
     hidden = model.init_hidden(eval_batch_size)
-    data, targets = get_batch(data_source, 0, bptt,evaluation=True)
+    data, targets = get_batch(data_source, index, bptt,evaluation=True)
     output, hidden = model(data, hidden)
     output_flat = output.view(-1, ntokens)
 
-    # print('output size:', output.data.size())
-    # print('targets size:', targets.size())
-    # print('output size:', output.data.size())
-    # print('output_flat size:', output_flat.data.size())
-    # print output.data[0]
-    output_words = []
-    target_words = []
-    # print targets
-    for i in range(bptt):
-        word_output_vector = output_flat.data[5+i*eval_batch_size]
-        word_index = word_output_vector.max(0)[1]
-        word_output = idx2word[int(word_index)]
-        
-        target_index = targets[5+i*eval_batch_size]
-        # print "target index %d" %(target_index)
-        target_word = idx2word[int(target_index)]
-        # print word_index
+    required_word_position, batch_position = check_tagret(targets,required_word_index,position,eval_batch_size,bptt)
+    if required_word_position is not None:
+        output_words = []
+        target_words = []
+        # print targets
+        for i in range(bptt):
+            word_output_vector = output_flat.data[batch_position+i*eval_batch_size]
+            word_index = word_output_vector.max(0)[1]
+            word_output = idx2word[int(word_index)]
+            
+            target_index = targets[batch_position+i*eval_batch_size]
+            # print "target index %d" %(target_index)
+            target_word = idx2word[int(target_index)]
+            # print word_index
 
-        output_words.append(word_output)
-        target_words.append(target_word)
-        # print "output word: %s" %() 
-        # print "target word: %s" %() 
+            output_words.append(word_output)
+            target_words.append(target_word)
+            # print "output word: %s" %() 
+            # print "target word: %s" %() 
+            if i == required_word_position:
+                print "for target word %s" %(target_word)
+                m = nn.Softmax(dim=0)
+                input = torch.autograd.Variable(word_output_vector,requires_grad=False)
+                softmax_output = m(input).data
+                topk_values, topk_indecies = torch.topk(softmax_output,10,0)
+                # topk_values = topk_values.view(-1,1)
+                # topk_indecies = topk_indecies.view(-1,1)
+                for k in range(topk_values.size(0)):
+                    word = idx2word[topk_indecies[k]]
+                    possiblility = topk_values[k]
+                    print "\tword:{0} possiblility:{1:.3f}%".format(word,possiblility)
 
-    print "output words: %s" %(" ".join(output_words))
-    print '-'*10
-    print "targetwords: %s" %(" ".join(target_words))
-    print output_flat.data[9]
+
+        print "output words: %s" %(" ".join(output_words))
+        print "targetwords: %s" %(" ".join(target_words))
+        # print output_flat.data[9]
+        print '-'*10
+        return True
+    else:
+        return False
+
+
+    
     
 
 def main():
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument("model_dir_path")
+    parser.add_argument("--word","-w",default="Nepal")
+    parser.add_argument("--position","-p",type=int,default=5,
+                    help="""
+                        the position that the required word must be higher than
+                    """)
     parser.add_argument("-a","--all", action='store_true',
                     help='if specified, evaluate all test data')
     args=parser.parse_args()
@@ -160,8 +190,22 @@ def main():
             test_loss, math.exp(test_loss)))
         print('=' * 89)
     else:
-        evaluate_small_sample(test_data,model,corpus,training_config.bptt,eval_batch_size)
-
+        past_batch_index = set()
+        number_of_sentence = 0
+        word_index = corpus.dictionary.word2idx[args.word]
+        print "The word index is:%d" %(word_index)
+        print corpus.dictionary.idx2word[word_index]
+        possible_indecies = range(0, test_data.size(0) - 1, training_config.bptt)
+        while number_of_sentence < 5:
+            i = randint(0, len(possible_indecies))
+            batch_index = possible_indecies[i]
+            # print "batch index: %d" %(batch_index)
+            succeed  = evaluate_small_sample(test_data,model,corpus,
+                                             training_config.bptt,
+                                             eval_batch_size, word_index,
+                                             args.position, batch_index)
+            if succeed:
+                number_of_sentence += 1
 
 if __name__=="__main__":
     main()
