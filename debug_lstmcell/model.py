cb78cb5be6382f8b945f3a8b49ac70b6069d4d02
Kuang <lukuang1989@gmail.com>
2018-03-13 00:35:27 +0000

    fix typo

--- debug_lstmcell/model.py
+++ debug_lstmcell/model.py
@@ -70,9 +70,11 @@ class RNNModel(nn.Module):
 
         output = self.drop(output)
         hidden = (new_hx, new_cx)
-        decoded = self.decoder(output.view(output.size(0)*output.size(1), output.size(2)))
         print output.size()
-        print self.decoder.size()
+        print self.decoder
+	decoded = self.decoder(output.view(output.size(0)*output.size(1), output.size(2)))
+        #print output.size()
+        #print self.decoder.size()
         return decoded.view(output.size(0), output.size(1), decoded.size(1)), hidden
 
     def init_hidden(self, bsz):
